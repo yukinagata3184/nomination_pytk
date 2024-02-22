566e9683e4466b2e23ebf61048ec8a1dd7c44178
yukinagata3184 <yuki.nagata3184@gmail.com>
2024-02-22 14:27:16 +0900

    #18 [Add] place label on the canvas.

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main():
     """
     gui = MkGUI(title="nomination", is_resize=False, width=1000, height=600)
     gui.place_img(img_path="img/teacher_india_man.png", x_axis=150, y_axis=250)
+    gui.place_label(text="選ばれたのは", font="Times New Roman", font_size=40,
+                    bg_color="#f0f0f0", x_axis=300, y_axis=0)
+    gui.place_label(text="？？？", font="Times New Roman", font_size=70,
+                        bg_color="white", x_axis=300, y_axis=75)
     gui.root.mainloop()
 
 if __name__ == "__main__":
